116583a032e395d1dc55e0c431817007563dab7d
lycis <daniel@deder.at>
2014-01-26 01:00:15 +0100

    extended server info

--- komorebi-core/src/main/java/org/komorebi/core/requests/ServerInfo.java
+++ komorebi-core/src/main/java/org/komorebi/core/requests/ServerInfo.java
@@ -1,5 +1,9 @@
 package org.komorebi.core.requests;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -10,17 +14,35 @@ import org.komorebi.core.Version;
 
 /**
  * This request provides current information about the server.
+ * 
  * @author lycis
- *
+ * 
  */
 @Path("serverInfo")
 public class ServerInfo {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String get(){
+	public String get() {
 		JSONObject json = new JSONObject();
+
+		// version
 		json.put("version", Version.versionString());
+
+		// start time
+		RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+		json.put("startTime", rb.getStartTime());
+
+		// readable uptime
+		long uptime = rb.getUptime();
+		json.put("uptimeReadable", String.format(
+				"%d days, %d min, %d sec",
+				TimeUnit.DAYS.toDays(uptime),
+				TimeUnit.MILLISECONDS.toMinutes(uptime) - TimeUnit.DAYS.toSeconds(TimeUnit.MINUTES.toDays(uptime)),
+				TimeUnit.MILLISECONDS.toSeconds(uptime) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(uptime))
+				)
+		);
+
 		return json.toString();
 	}
 }
